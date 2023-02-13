e3fdd5b118d405b6adf7952f73f93972f8e95a62
pritam962 <117460699+pritam962@users.noreply.github.com>
2023-02-13 22:21:02 +0530

    Add files via upload

---
+++ assignment_1_2.java
@@ -0,0 +1,18 @@
+
+public class assignment_1_2 {
+
+	public static void main(String[] args) {
+		int n=4;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				
+				System.out.print(i);
+			}
+			
+		System.out.println();
+		}
+	}
+
+}
