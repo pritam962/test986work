ab664f95d45d5c172584a4eedc41b4b77c757813
pritam962 <117460699+pritam962@users.noreply.github.com>
2023-05-13 23:27:24 +0530

    Add files via upload

--- launch.java
+++ launch.java
@@ -1,26 +1,55 @@
-package bubbleSortAssignment;
+package mergesort_assignment_2;
+
 public class launch {
-	public static void main(String []args) {
-		
-	
-	int [] ar= {10,40,50,20,70,5};			
-  for(int i=0;i<ar.length;i++)
-  {
-	  for(int j=1;j<ar.length-i;j++)
-	  {
-		  if(ar[j]<ar[j-1])
-		  {
-			  int temp=ar[j];
-			  ar[j]=ar[j-1];
-			  ar[j-1]=temp;	
-		  } 
-	  } 
-  }
-  for(int ele:ar)
-  {
-	  System.out.print(ele+" ");
-  }
-  
-}
-}
-	
+	    public static void display(int[] arr, int size)
+	    {
+	        for(int i = 0; i < size; i++) {
+	            System.out.print(arr[i] + " ");
+	        }
+	        System.out.println();
+	    }
+	    public static void main(String[] args)
+	    {
+	        int[] a = {12, 11, 13, 5, 6, 7 };
+
+	        int size = a.length;
+	        display(a, size);
+
+	        quickSort(a, 0, size - 1);
+	        display(a, size);
+	    }
+	    
+	    static void swap(int[] arr, int i, int j)
+	    {
+	        int temp = arr[i];
+	        arr[i] = arr[j];
+	        arr[j] = temp;
+	    }
+	    static void quickSort(int[] arr, int low, int high)
+	    {
+	        if (low < high)
+	        {
+	            int indexPI  = partition(arr, low, high);
+
+	            quickSort(arr, low, indexPI  - 1);  
+	            quickSort(arr, indexPI  + 1, high); 
+	        }
+	    }
+	    static int partition(int[] arr, int low, int high)
+	    {
+	        int pivot = arr[high];
+	        int swapIndex  = (low - 1);   
+	        for (int j = low; j <= high- 1; j++)
+	        {
+	            if (arr[j] < pivot)
+	            {
+	                swapIndex++;    
+	                swap(arr, swapIndex, j);
+	            }
+	        }
+	        swap(arr, swapIndex + 1, high);
+
+	        return (swapIndex + 1);
+	    }
+	}
+
