73428e51cf438656f3a77c14f6aea4d8f2ff3315
pritam962 <117460699+pritam962@users.noreply.github.com>
2023-05-13 23:26:21 +0530

    Add files via upload

--- launch.java
+++ launch.java
@@ -1,32 +1,26 @@
-
+package bubbleSortAssignment;
 public class launch {
-	static boolean isSubset(int arr1[], int arr2[], int m,int n)
-	{
-		int i = 0;
-		int j = 0;
-		for (i = 0; i < n; i++) {
-			for (j = 0; j < m; j++)
-				if (arr2[i] == arr1[j])
-					break;
-			if (j == m)
-				return false;
-		}
-		return true;
-	}
-	public static void main(String args[])
-	{
-		int arr1[] = { 11, 1, 13, 21, 3, 7 };
-		int arr2[] = { 11, 3, 7, 1 };
-
-		int m = arr1.length;
-		int n = arr2.length;
-
-		if (isSubset(arr1, arr2, m, n))
-			System.out.print("arr2[] is "
-							+ "subset of arr1[] ");
-		else
-			System.out.print("arr2[] is "
-							+ "not a subset of arr1[]");
-	}
+	public static void main(String []args) {
+		
+	
+	int [] ar= {10,40,50,20,70,5};			
+  for(int i=0;i<ar.length;i++)
+  {
+	  for(int j=1;j<ar.length-i;j++)
+	  {
+		  if(ar[j]<ar[j-1])
+		  {
+			  int temp=ar[j];
+			  ar[j]=ar[j-1];
+			  ar[j-1]=temp;	
+		  } 
+	  } 
+  }
+  for(int ele:ar)
+  {
+	  System.out.print(ele+" ");
+  }
+  
 }
-
+}
+	
